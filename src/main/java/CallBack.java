b7862293ba3e80025436ed4d6c2761859daacff6
Dinith Sadeepa Kodithuwakku <dinithsadeepaofficial98@gmail.com>
2019-07-12 11:20:58 +0530

    add logs

--- src/main/java/CallBack.java
+++ src/main/java/CallBack.java
@@ -1,18 +1,24 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+
 public class CallBack implements MqttCallback {
+
+    private static final Logger LOGGER = LogManager.getLogger(CallBack.class);
 
     public void connectionLost(Throwable throwable) {
 
     }
 
     public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+        LOGGER.info("=> message : " +topic +"mqtt m : "+mqttMessage);
         System.out.println(topic + mqttMessage);
     }
 
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-
+        LOGGER.info("=> delivery : " +iMqttDeliveryToken);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -6,6 +8,7 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import java.util.UUID;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(CallBack.class);
     public static void main(String[] args) {
         try {
             MqttAsyncClient mqttAsyncClient = new MqttAsyncClient("tcp://13.127.205.221", UUID.randomUUID().toString());
@@ -16,6 +19,7 @@ public class Main {
             token.waitForCompletion();
             mqttAsyncClient.subscribe("/ceyentra/topic1", 0);
             mqttAsyncClient.publish("/ceyentra/topic2", new MqttMessage("hello1".getBytes()));
+            LOGGER.info("=> started");
         } catch (MqttException e) {
             e.printStackTrace();
         }
